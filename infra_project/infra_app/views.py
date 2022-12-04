9a3c4c3f614c3db243a48f73c5efc7289c8e3f3a
Naidenov1898 <naiden1898@yandex.ru>
2022-12-04 23:16:24 +0300

    123

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return Http404('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return Http404('А это вторая страница')
